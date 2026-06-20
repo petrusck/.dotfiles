8eadc052a61898f8b295f4b8842a5be791a21a37
Pere Bohigas <pere.bohigas@gmail.com>
2026-06-20 15:50:06 +0200

    Add GitUI configuration

---
+++ gitui/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+TOOL_DIR="${DOTFILES_PATH:=$PWD}/gitui"
+
+[ ! -d "$HOME/.config/gitui" ] && mkdir -p "$HOME/.config/gitui"
+ln -sf "$TOOL_DIR/key_config.ron" "$HOME/.config/gitui/key_config.ron"
