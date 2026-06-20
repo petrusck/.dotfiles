9f0d2a23102bcf435d89ae82649c5b1c5752debc
Pere Bohigas <pere.bohigas@gmail.com>
2026-06-20 20:56:33 +0200

    Add Zsh configuration

---
+++ zsh/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+TOOL_DIR="${DOTFILES_PATH:=$PWD}/zsh"
+
+ln -sf "$TOOL_DIR/zshenv" "$HOME/.zshenv"
+
+[ ! -d "$HOME/.config/zsh" ] && mkdir -p "$HOME/.config/zsh"
+ln -sf "$TOOL_DIR/zprofile" "$HOME/.config/zsh/.zprofile"
+ln -sf "$TOOL_DIR/zshrc" "$HOME/.config/zsh/.zshrc"
+ln -sf "$TOOL_DIR/zsh_aliases" "$HOME/.config/zsh/zsh_aliases"
+ln -sfn "$TOOL_DIR/zsh_additional_configuration" "$HOME/.config/zsh/zsh_additional_configuration"
+ln -sfn "${DOTFILES_PATH:=$PWD}/shell_scripts/zsh" "$HOME/.config/zsh/zsh_functions"
+ln -sfn "${DOTFILES_PATH:=$PWD}/shell_scripts/bash" "$HOME/.config/zsh/bash_functions"
+
+touch ~/.hushlogin # disable the "Last Login" message on new terminal session
