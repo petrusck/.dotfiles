4fb67fe07fb4947af587aaa6e345e779c2d9724e
Pere Bohigas <pere.bohigas@gmail.com>
2026-06-20 16:45:02 +0200

    Add mise configuration

---
+++ mise/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+TOOL_DIR="${DOTFILES_PATH:=$PWD}/mise"
+
+[ ! -d "$HOME/.config/mise" ] && mkdir -p "$HOME/.config/mise"
+ln -sf "$TOOL_DIR/config.toml" "$HOME/.config/mise/config.toml"
+command -v mise &>/dev/null && mise trust "$HOME/.config/mise/config.toml"
