ec54d405c8a6f7ac75281523b31324601c017bd3
Pere Bohigas <pere.bohigas@gmail.com>
2026-06-20 16:35:31 +0200

    Add Leader Key configuration

---
+++ leader_key/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+TOOL_DIR="${DOTFILES_PATH:=$PWD}/leader_key"
+
+[ ! -d "$HOME/.config/leader_key" ] && mkdir -p "$HOME/.config/leader_key"
+ln -sf "$TOOL_DIR/config.json" "$HOME/.config/leader_key/config.json"
+
+# Setup config directory in Preferences... > Advanced and Reload from file in Preferences... > General
